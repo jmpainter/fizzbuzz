cfb52fe613e029da24f673c19cec6c664a741099
Josh Painter <jpainter4@gmail.com>
2015-01-05 15:48:12 -0800

    fixed space issue

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
 	var string = "";
 	for(count = 1; count <= 100; count++) {
 		string = string + '<p>';
-		if(count % 3 == 0 || count % 5 == 0) {
+		if(count % 3 == 0 && count % 5 == 0) {
+			string = string + "fizz buzz";
+		}
+		else if(count % 3 == 0 || count % 5 == 0) {
 			if(count % 3 == 0) {
-				string = string + "fizz ";
+				string = string + "fizz";
 			}
 			if(count % 5 == 0) {
-				string = string + "buzz ";
+				string = string + "buzz";
 			}
 		}
 		else {
