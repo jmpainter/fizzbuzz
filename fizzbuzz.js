2e7b405b826258672f283142f305a8799926923d
Josh Painter <jpainter4@gmail.com>
2015-01-05 15:42:42 -0800

    create fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var count;
+	var string = "";
+	for(count = 1; count <= 100; count++) {
+		string = string + '<p>';
+		if(count % 3 == 0 || count % 5 == 0) {
+			if(count % 3 == 0) {
+				string = string + "fizz";
+			}
+			if(count % 5 == 0) {
+				string = string + "buzz";
+			}
+		}
+		else {
+			string = string + count;
+		}
+		string = string + '</p>';
+	}
+	$('body').html(string);
+});
