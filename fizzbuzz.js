dc056cbc44dd8385014568b591e01a5b4e593f77
Josh Painter <jpainter4@gmail.com>
2015-01-05 17:27:23 -0800

    removed first if statement

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
 	var string = "";
 	for(count = 1; count <= 100; count++) {
 		string = string + '<p>';
-		if(count % 3 == 0 && count % 5 == 0) {
-			string = string + "fizz buzz";
-		}
-		else if(count % 3 == 0 || count % 5 == 0) {
+
+		if(count % 3 == 0 || count % 5 == 0) {
 			if(count % 3 == 0) {
-				string = string + "fizz";
+				string = string + "fizz ";
 			}
 			if(count % 5 == 0) {
 				string = string + "buzz";
